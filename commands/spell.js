b864c0db2b3240aa9eb899cebc40b09c6b4a5381
Chris <orj.tsuk@gmail.com>
2022-03-13 23:08:33 -0400

    pushing fix to public spell message, and cleaning up console logs.

--- commands/spell.js
+++ commands/spell.js
@@ -37,7 +37,7 @@ module.exports = {
 
 			// Below is to deal with messages that are too long. Discord has a max character length of 2000.
 			// So we count the message length, and if it is greater than the max char length we cut the message
-			// off by 100 characters, push it to a new array, and rejoin it, and let the player know with a message at the end. 
+			// off by a number of characters, push it to a new array, and rejoin it, and let the player know with a message at the end. 
 
 			let total = 0
 			const max = 2000
@@ -47,31 +47,31 @@ module.exports = {
 			}
 			const diff = total - max
 			const tooLong = ' Message cut off due to length...'
+			// if too long, we need to add the %20 for spaces in the web address
 			const webFilter = query.replace(/\s/g, "%20")
 			const tooLongWeb = `https://5e.tools/spells.html#${webFilter}_phb`
 
 			let trunc = []
-			for (let i = 0; i < max - 1900; i += 1) {
+			for (let i = 0; i < max - 700; i += 1) {
 				trunc.push(desc[i])
 			}
 			const fixed = trunc.join('')
 
 			console.log(`Public: ${query}`)
-			console.log(diff, trunc.length)
-			
-			const testing = (`**${data.name}**:\n**School**: ${school} \n\n**Range**: ${data.range} \n**Duration**: ${data.duration} \n${`${fixed}.... **${tooLong}**`}\n**Goto ${tooLongWeb} For full the full description.**`)
-			
-			console.log(testing.length)
-			console.log(fixed.length)
-			if (damage  || heal && total < 2000) {
-				interaction.editReply(`**${data.name}**:\n**School**: ${school}\n**Range**: ${data.range} \n**Duration**: ${data.duration} \n${desc} \n ${heal ? '**Healing**' : '**Damage**'}: \n\t\t\t\t\t${heal ? spellData(heal): spellData(damage)} `);
-			} else if (!damage || !heal && total < 2000) {
-				interaction.editReply(`**${data.name}**:\n**School**: ${school}\n**Range**: ${data.range} \n**Duration**: ${data.duration} \n${desc}`);
-			} else if (damage || heal && total > 2000) {
-				interaction.editReply(`**${data.name}**:\n**School**: ${school}\n**Range**: ${data.range} \n**Duration**: ${data.duration} \n${`${fixed}.... **${tooLong}**`}\n**Goto ${tooLongWeb} For full the full description.**`);
-			} else if (!damage  || !heal && total > 2000) {
-				interaction.editReply(`**${data.name}**:\n**School**: ${school} \n\n**Range**: ${data.range} \n**Duration**: ${data.duration} \n${`${fixed}.... **${tooLong}**`}\n**Goto ${tooLongWeb} For full the full description.**`);
-			}
+
+            if (total < 2000) {
+                if (damage || heal) {
+                    interaction.editReply(`**${data.name}**:\n**School**: ${school}\t**Range**: ${data.range} \t**Duration**: ${data.duration} \n${desc} \n ${heal ? '**Healing**' : '**Damage**'}: \n\t\t\t\t\t${heal ? spellData(heal) : spellData(damage)} `);
+                } else if (!damage || !heal) {
+                    interaction.editReply(`**${data.name}**:\n**School**: ${school}\t**Range**: ${data.range} \t**Duration**: ${data.duration} \n${desc}`);
+                }
+            } else if (total > 2000) {
+                if (damage || heal) {
+                    interaction.editReply(`**${data.name}**\n**School**: ${school}\t**Range**: ${data.range} \t**Duration**: ${data.duration} \n${`${fixed}.... **${tooLong}**`}\n**Goto ${tooLongWeb} For full the full description.**`);
+                } else if (!damage || !heal) {
+                    interaction.editReply(`**${data.name}**:\n**School**: ${school}\t**Range**: ${data.range} \t**Duration**: ${data.duration} \n${`${fixed}.... **${tooLong}**`}\n**Goto ${tooLongWeb} For full the full description.**`);
+                }
+            }
 		}
 	},
 };
--- commands/wspell.js
+++ commands/wspell.js
@@ -24,16 +24,16 @@ module.exports = {
             const heal = data?.heal_at_slot_level
             const desc = data.desc.join('\n\n')
             const school = data?.school?.name
-                const spellData = (spell) => {
-                    let dmg = []
-                    for (const key in spell) {
-                        if (Object.hasOwnProperty.call(spell, key)) {
-                            const element = spell[key];
-                            dmg.push(`**${key}**: ${element} \n`)
-                        }
+            const spellData = (spell) => {
+                let dmg = []
+                for (const key in spell) {
+                    if (Object.hasOwnProperty.call(spell, key)) {
+                        const element = spell[key];
+                        dmg.push(`**${key}**: ${element} \n`)
                     }
-                    return dmg.join('\t\t\t\t\t')
                 }
+                return dmg.join('\t\t\t\t\t')
+            }
             // Below is to deal with messages that are too long. Discord has a max character length of 2000.
             // So we count the message length, and if it is greater than the max char length we cut the message
             // off by a number of characters, push it to a new array, and rejoin it, and let the player know with a message at the end. 
@@ -48,35 +48,29 @@ module.exports = {
             const tooLong = ' Message cut off due to length...'
             // if too long, we need to add the %20 for spaces in the web address
             const webFilter = query.replace(/\s/g, "%20")
-			const tooLongWeb = `https://5e.tools/spells.html#${webFilter}_phb`
+            const tooLongWeb = `https://5e.tools/spells.html#${webFilter}_phb`
 
-			let trunc = []
-			for (let i = 0; i < max - 700; i += 1) {
-				trunc.push(desc[i])
-			}
-			const fixed = trunc.join('')
+            let trunc = []
+            for (let i = 0; i < max - 700; i += 1) {
+                trunc.push(desc[i])
+            }
+            const fixed = trunc.join('')
 
-            if(total < 2000){
-                if(damage || heal){
-                    console.log(`TOTAL: ${total}`)
-                    console.log('dmg or heal less')
-                    interaction.editReply(`**${data.name}**:\t**School**: ${school}\t**Range**: ${data.range} \t**Duration**: ${data.duration} \n${desc} \n ${heal ? '**Healing**' : '**Damage**'}: \n\t\t\t\t\t${heal ? spellData(heal): spellData(damage)} `);
-                }else if (!damage || !heal){
-                    console.log(`TOTAL: ${total}`)
-                    console.log('!dmg or !heal less')
-                    interaction.editReply(`**${data.name}**:\t**School**: ${school}\t**Range**: ${data.range} \t**Duration**: ${data.duration} \n${desc}`);
+            console.log(`Ephemeral: ${query}`)
+
+            if (total < 2000) {
+                if (damage || heal) {
+                    interaction.editReply(`**${data.name}**:\n**School**: ${school}\t**Range**: ${data.range} \t**Duration**: ${data.duration} \n${desc} \n ${heal ? '**Healing**' : '**Damage**'}: \n\t\t\t\t\t${heal ? spellData(heal) : spellData(damage)} `);
+                } else if (!damage || !heal) {
+                    interaction.editReply(`**${data.name}**:\n**School**: ${school}\t**Range**: ${data.range} \t**Duration**: ${data.duration} \n${desc}`);
                 }
-            }else if (total > 2000){
-                if(damage || heal){
-                    console.log(`TOTAL: ${total}`)
-                    console.log('dmg or heal great')
-                    interaction.editReply(`**${data.name}**\t**School**: ${school}\t**Range**: ${data.range} \t**Duration**: ${data.duration} \n${`${fixed}.... **${tooLong}**`}\n**Goto ${tooLongWeb} For full the full description.**`);
-                }else if(!damage || !heal){
-                    console.log(`TOTAL: ${total}`)
-                    console.log('!dmg or !heal greater')
-                    interaction.editReply(`**${data.name}**:\t**School**: ${school}\t**Range**: ${data.range} \t**Duration**: ${data.duration} \n${`${fixed}.... **${tooLong}**`}\n**Goto ${tooLongWeb} For full the full description.**`);
+            } else if (total > 2000) {
+                if (damage || heal) {
+                    interaction.editReply(`**${data.name}**\n**School**: ${school}\t**Range**: ${data.range} \t**Duration**: ${data.duration} \n${`${fixed}.... **${tooLong}**`}\n**Goto ${tooLongWeb} For full the full description.**`);
+                } else if (!damage || !heal) {
+                    interaction.editReply(`**${data.name}**:\n**School**: ${school}\t**Range**: ${data.range} \t**Duration**: ${data.duration} \n${`${fixed}.... **${tooLong}**`}\n**Goto ${tooLongWeb} For full the full description.**`);
                 }
             }
-		}
-	},
+        }
+    },
 };
