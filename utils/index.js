259db2b4723d731fd093534fcddf28b0693b716b
Chris Weiskopf <chris@chriscancode.io>
2023-04-23 14:34:08 -0400

    adding suggestions for similar results on a failed search

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,5 @@
+const stringSimilarity = require("string-similarity");
+
 const queryData = async (spell, dataArray) => {
   const searchTerm = spell.toLowerCase().replace(/'/g, "");
   const exactMatch = dataArray.find(
@@ -9,10 +11,16 @@ const queryData = async (spell, dataArray) => {
   }
 
   const suggestions = dataArray
-    .filter((item) =>
-      item.name.toLowerCase().replace(/'/g, "").includes(searchTerm)
-    )
+    .map((item) => ({
+      name: item.name,
+      similarity: stringSimilarity.compareTwoStrings(
+        searchTerm,
+        item.name.toLowerCase().replace(/'/g, "")
+      ),
+    }))
+    .sort((a, b) => b.similarity - a.similarity)
     .slice(0, 5) // Limit the number of suggestions to 5, you can adjust this number
+    .filter((item) => item.similarity > 0.3) // You can adjust the threshold for similarity
     .map((item) => item.name);
 
   return { exact: null, suggestions };
