9a952ec8cd75c4e44ddee2463a1d5f60b57f6ca3
Chris Weiskopf <chris@chriscancode.io>
2023-04-23 15:52:31 -0400

    adding buttons to main commands

--- commands/spell.js
+++ commands/spell.js
@@ -3,6 +3,7 @@ const { MessageEmbed } = require("discord.js");
 const wait = require("node:timers/promises").setTimeout;
 const newUnlisted = require("../utils/new_unlisted");
 const localQuery = require("../utils/index");
+const getSearchTerm = require("../utils/getSearchTerm");
 
 module.exports = {
   data: new SlashCommandBuilder()
